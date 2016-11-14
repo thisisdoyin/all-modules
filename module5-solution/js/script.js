77f695af1e0701895d8d342795acfa3910dba473
Andrea Adebowale <andrea.ade2@gmail.com>
2016-11-14 01:33:00 -0500

    Umm

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -62,7 +62,6 @@ var switchMenuToActive = function () {
 
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
-
 // TODO: STEP 0: Look over the code from
 // *** start ***
 // to
@@ -101,7 +100,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory (categories);
+      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -114,19 +113,19 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-            
-            var homeHtmlToInsertIntoMainPage = 
-            insertProperty(chosenCategoryShortName) {
-              $dc.loadMenuItems("#main-content");
+
+            var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName", "'" + chosenCategoryShortName + "'");
+             
           
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-              insertHTML ("#main-content", categoriesViewHtml); // ....
-             }
-          }
+             insertHTML ("#main-content", homeHtmlToInsertIntoMainPage); // ....
+             
+          
     },
+
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
 
