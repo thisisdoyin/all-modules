b20033beec3cc109659270fb34fc31656ca8173f
Andrea Adebowale <andrea.ade2@gmail.com>
2016-11-14 00:12:26 -0500

    difficult

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory;
+      var chosenCategoryShortName = chooseRandomCategory (categories);
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -114,15 +114,20 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
-
+            
+            var homeHtmlToInsertIntoMainPage = 
+            insertProperty(chosenCategoryShortName) {
+              $dc.loadMenuItems("#main-content");
+          
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
+              insertHTML (){
 
+              }; // ....
+             }
+          }
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
